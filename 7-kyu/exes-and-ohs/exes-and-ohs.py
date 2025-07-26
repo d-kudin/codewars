62f760b2beceef2efc1abd6568545a7643b56ef2
d-kudin <97442072+d-kudin@users.noreply.github.com>
2025-07-26 20:10:35 +0200

    Update solution - CodeHub

--- 7-kyu/exes-and-ohs/exes-and-ohs.py
+++ 7-kyu/exes-and-ohs/exes-and-ohs.py
@@ -1,9 +1,3 @@
 def xo(s):
-    count1 = 0
-    count2 = 0
-    for i in s:
-        if i in 'xX':
-            count1 += 1
-        elif i in 'oO':
-            count2 += 1
-    return count1 == count2
+    s = s.lower()
+    return s.count('x') == s.count('o')
